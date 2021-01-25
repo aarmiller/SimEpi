89016182cd292c58f128161e478d327da13b116e
aarmiller <aaron-miller@uiowa.edu>
2021-01-25 13:18:32 -0600

    Add Overview document

---
+++ admin/Overview.md
@@ -0,0 +1,20 @@
+Course Outline
+================
+Aaron Miller
+1/25/2021
+
+## Course Outline
+
+This document provides an outline of the topics, scripts and associated
+readings that will be covered as we move through the class. Note: this
+document will continue to be updated as week move through the class.
+
+## Day 1 - 1/26/2021
+
+Topics Covered
+
+-   Overview of course
+
+-   Intro to simulation slides
+
+-   Basics of R script
---
+++ in_class_scripts/basics.R
@@ -0,0 +1,102 @@
+
+# This script contains examples for a basic review of R
+
+#### Some starting points ####
+
+# See Chapters 4,6 of R4DS
+
+
+##################################
+#### R scripts and commenting ####
+##################################
+
+# Use # to comment code
+# It is important to leave yourself notes
+
+#################################
+#### Using R as a calculator ####
+#################################
+
+
+###########################
+#### Object assignment ####
+###########################
+
+# Why <- is preferred over =
+
+# Naming conventions - Consistency is important for efficient programming 
+
+# Illegal Names
+
+
+#########################################
+#### Workspace and working directory ####
+#########################################
+
+# clearing objects
+
+# clearing all objects
+
+############################################
+#### Very Basic Data Types & Structures ####
+############################################
+
+# basic data types
+
+#vector
+
+# list
+
+# data.frame
+
+# Note: a data.frame is a special type of list
+
+# missing values
+
+###################
+#### Functions ####
+###################
+
+# basic functions
+
+
+################
+#### Base R ####
+################
+
+
+
+#############################
+#### installing packages ####
+#############################
+
+#installing packages
+
+# updating packages
+
+# loading packages
+
+# calling functions from within packages
+
+# installing packages from github
+
+
+############################
+#### Logical Operations ####
+############################
+
+
+
+###########################
+#### Internal datasets ####
+###########################
+
+
+####################
+#### Help Files ####
+####################
+
+
+# examples
+
+
