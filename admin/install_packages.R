3e65fbaaf1177884ece5a4a703c6aaa6eedfc0eb
aarmiller <34134737+aarmiller@users.noreply.github.com>
2021-01-26 16:22:28 -0600

    Add some test scripts

--- admin/install_packages.R
+++ admin/install_packages.R
@@ -4,7 +4,7 @@ install.packages("tidyverse")
 
 ## or we can install packages from a vector of names
 list_of_packages <- c("microbenchmark","deSolve","rootSolve","Rcpp","EpiModel",
-                      "lobstr","statnetWeb","tidymodels","keras","caret")
+                      "lobstr","statnetWeb","tidymodels","keras","caret","bit64")
 
 install.packages(list_of_packages,dependencies = TRUE)
 
