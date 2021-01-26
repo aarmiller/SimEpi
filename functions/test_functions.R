de68d88bc4a3b5a8d79d4b138f26f2553091d0e2
aarmiller <34134737+aarmiller@users.noreply.github.com>
2021-01-26 15:57:26 -0600

    Create test_functions.R
    Added a test function

---
+++ functions/test_functions.R
@@ -0,0 +1,8 @@
+
+# This functions file is just for demonstration and testing purposes
+
+test_function <- function(){
+  my_name <- readline(prompt = "What is your name? ")
+  print(paste("Hello",my_name))
+}
+
