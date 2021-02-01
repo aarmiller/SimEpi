706d53e8c2863b074e6ca3f8f52d7fb126a6ba5d
aarmiller <34134737+aarmiller@users.noreply.github.com>
2021-01-31 22:48:18 -0600

    old icd package install

--- admin/install_packages.R
+++ admin/install_packages.R
@@ -4,10 +4,12 @@ install.packages("tidyverse")
 
 ## or we can install packages from a vector of names
 list_of_packages <- c("microbenchmark","deSolve","rootSolve","Rcpp","EpiModel",
-                      "lobstr","statnetWeb","tidymodels","keras","caret","bit64",
-                      "icd")
+                      "lobstr","statnetWeb","tidymodels","keras","caret","bit64")
 
 install.packages(list_of_packages,dependencies = TRUE)
+
+## old icd package
+devtools::install_version("icd", version = "4.0.9", repos = "http://cran.us.r-project.org")
 
 # remove the package list
 rm(list=list_of_packages)
