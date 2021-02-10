1f248aba19c709925746e903ef1daca5d34eda93
aarmiller <34134737+aarmiller@users.noreply.github.com>
2021-02-10 12:18:21 -0600

    Update Week3 assignment

---
+++ assignments/week3_template.R
@@ -0,0 +1,137 @@
+
+############################################################
+### Assignment 1 - Simulation In Epidemiology Spring 2021
+### Due Date: 2/18/2021
+############################################################
+
+### Your Name: 
+
+library(tidyverse)
+library(lubridate)
+#library(zoo) # optional
+
+#############################
+#### Load & Process Data ####
+#############################
+
+
+
+#################################
+#### Plot COVID trends in IA ####
+#################################
+
+## Plot Deaths across time -----------------------------------------------------
+
+## Cumulative Deaths
+
+
+
+## New Daily Deaths
+
+
+
+## Weekly Average Number of Deaths
+
+
+
+## Moving Average Number of Deaths
+
+
+
+## Positivity Rates ------------------------------------------------------------
+
+## Histogram of Daily Positivity
+
+
+
+## Histogram of Weekly Positivity
+
+
+
+## Boxplot of Weekly Positivity
+
+
+
+## Violin plot of Weekly vs Daily Positivity
+
+
+
+## Cases and Deaths ------------------------------------------------------------
+
+## Total Cases and Deaths across Time using color
+
+
+
+## Normalized Total Cases and Deaths across Time using color
+
+
+
+## Total Cases and Deaths across Time using facets
+
+
+
+#######################################
+#### Plotting Trends Across States ####
+#######################################
+
+## Iowa and Neightbors ---------------------------------------------------------
+
+## Boxplot of Weekly Positivity Rates 
+
+
+
+
+## Line Plot of New Deaths Reported Across Time using color
+
+
+
+
+## Line Plot of New Deaths Reported Across Time using facets
+
+
+
+
+## Analyze State Population vs Cases/Mortality ---------------------------------
+
+## Scatterplot of total cases with linear trend line
+
+
+
+
+## Scatterplot of total deaths with linear trend line
+
+
+
+
+## Scatterplot of total cases and deaths using facets
+
+
+
+
+## Optional: Which states had excess deaths exceeding expected based on pop?
+
+
+
+
+## Compare pandemic trends in Midwest to rest of US ----------------------------
+
+# Total Cases in Iowa and Neighboring States
+
+
+
+
+# Total Cases in Iowa and non-Neighboring States
+
+
+
+
+# Total Cases and Deaths in Iowa and Neighboring States
+
+
+
+
+# Total Cases and Deaths in Iowa and non-Neighboring States
+
+
+
+
