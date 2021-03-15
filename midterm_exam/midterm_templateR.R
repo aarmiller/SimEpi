488909fcdca42ea1fb679b65c320bd24487a6d39
aarmiller <34134737+aarmiller@users.noreply.github.com>
2021-03-15 12:55:19 -0500

    Create midterm_templateR.R

---
+++ midterm_exam/midterm_templateR.R
@@ -0,0 +1,91 @@
+
+############################################################
+### Midterm - Simulation In Epidemiology Spring 2021
+### Due Date: 3/22/2021
+### Name: <Your Name>
+############################################################
+
+
+
+#######################################
+#### Build Datasets for Simulation ####
+#######################################
+
+
+
+
+
+####################################
+#### Write Simulation Functions ####
+####################################
+
+
+## Initialization Function -----------------------------------------------------
+
+
+
+
+
+## Allocation Function ---------------------------------------------------------
+
+
+
+
+
+## Distribution Function -------------------------------------------------------
+
+
+
+
+
+## Loop to run simulation Function ---------------------------------------------
+
+
+
+
+
+###################################
+#### Add stochastic components ####
+###################################
+
+
+## Update Initialization Function ----------------------------------------------
+
+
+
+
+## Justification for stochastic component --------------------------------------
+
+# provide a brief summary justifying your choice of distribution for example
+
+
+
+
+######################################
+#### Evaluate Different Scenarios ####
+######################################
+
+
+## Create alternative allocation functions -------------------------------------
+
+
+# allocate_vaccine_pop65()
+
+
+
+# allocate_vaccine_equal()
+
+
+
+# allocate_vaccine_case_history()
+
+
+
+## Update run_sim() function to include allocation options ---------------------
+
+
+
+
+
+
+## Evaluate different outcomes across multiple trials --------------------------
