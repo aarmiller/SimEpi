9b149801950991611676ed8dc3510c1dca2e1ad2
aarmiller <34134737+aarmiller@users.noreply.github.com>
2021-04-27 10:49:22 -0500

    Update base_SIR_sim.R

--- in_class_scripts/part3/base_SIR_sim.R
+++ in_class_scripts/part3/base_SIR_sim.R
@@ -11,7 +11,31 @@ library(tidyverse)
 n_agents <- 1000           # Number of agents (patients or HCWs) to simulate
 n_days <- 100              # Number of days to run simulation
 
+# contact (note this usually comes from data)
+daily_contacts <- 3
+contact_prob <- daily_contacts/n_agents
 
+# transmission (defined by the disease)
+susceptibility <- .1
+infectivity <- .65      # this should be a function of duration
+
+# state transitions
+duration <- 7  # this may need to be stochastic
+
+# start off the simulator (seeding)
+init_infect <- 2
+
+## Parameter Data Structures ---------------------------------------------------
+
+# contact matrix
+cm <- matrix(contact_prob,n_agents,n_agents)
+#cm <- matrix(rnorm(mean=contact_prob,sd = 0.00001,8*8),8,8)
+# make diagonals 0
+diag(cm) <- 0
+# make symettric
+cm[lower.tri(cm)] <- t(cm)[lower.tri(cm)]
+
+cm[1:3,1:3]
 
 ########################################
 #### Simulation Procedure Functions ####
@@ -22,13 +46,41 @@ n_days <- 100              # Number of days to run simulation
 # Write a function that builds a set of agents for the simulator
 
 build_patients <- function(){
-  }
+  
+  infect_index <- sample(n_agents, size = init_infect)
+  
+  tibble(agent = 1:n_agents,
+         time = 1L,
+         state = ifelse(agent %in% infect_index, "I", "S"),
+         days_infected = ifelse(state == "I", 1L, 0L),
+         #days_infected = ifelse(state == "I", sample(1:duration,init_infect), 0L),
+         susceptibility = susceptibility,
+         transmisibility = infectivity,
+         trans_ind = NA)
+}
+
+build_patients() 
+
+build_patients() %>% filter(state=="I")
 
 ## Make Interactions -----------------------------------------------------------
 
 # Write a function that makes the patients interact with one another
 
 make_interactions <- function(){ 
+  
+  # Loop over agents
+  
+  # How to do this efficiently?
+  
+  # for each agent i
+  
+  #### for each agent j != i
+  
+  ####### draw contact with probability cm[i,j]
+  
+  ####### if this contact is drawn then update trans_indicator or trans_vector
+  
   }
 
 ## Draw Transmission Events ----------------------------------------------------
@@ -60,7 +112,7 @@ update_states <- function(){
 
 patients <- build_patients()
 
-for (i in 1:100) {
+for (i in 1:n_days) {
   
   make_interactions()
   
