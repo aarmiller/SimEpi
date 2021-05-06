575f2f5666b806e5e681294d10f94fa34c6ce2a5
aarmiller <34134737+aarmiller@users.noreply.github.com>
2021-05-06 09:20:08 -0500

    Update multi_types_contact_matrix.R

--- in_class_scripts/part3/multi_types_contact_matrix.R
+++ in_class_scripts/part3/multi_types_contact_matrix.R
@@ -75,3 +75,48 @@ cm
 cm[1:10,1:10]
 
 cm[790:800,790:800]
+
+
+## functionalize ---------------------------------------------------------------
+
+contact_probs <- matrix(c(0.004,0.005,0.005,0.0005),nrow = 2)
+
+row.names(contact_probs) <- c("H","P")
+colnames(contact_probs) <- c("H","P")
+
+contact_probs
+
+make_cm <- function(agent_types, n_types, contact_probs) {
+  
+  n_agents <- sum(n_types)
+  
+  agents <- rep(agent_types,times = n_types)
+  
+  ## HCW -> HCW ----------------------------------------------------------------
+  
+  cm1 <- matrix(rep(contact_probs[1,1],times = n_types[1]^2),nrow = n_types[1])
+  
+  ## Patients -> Patients --------------------------------------------------------
+  
+  cm2 <- matrix(rep(contact_probs[2,2],times = n_types[2]^2),nrow = n_types[2])
+  
+  ## Patients -> HCWs (0.05) -----------------------------------------------------
+  cm3 <- matrix(rep(contact_probs[2,1],times = n_types[1]*n_types[2]),nrow = n_types[2])
+  
+  # combine submatrices
+  cm <- rbind(cbind(cm1,t(cm3)),
+              cbind(cm3,cm2))
+  
+  # fix diagonal
+  diag(cm) <- 0
+  
+  # add names
+  row.names(cm) <- agents
+  colnames(cm) <- agents
+  
+  return(cm)
+}
+
+make_cm(agent_types = c("H","P"),
+        n_types = c(5,3),
+        contact_probs = contact_probs)
