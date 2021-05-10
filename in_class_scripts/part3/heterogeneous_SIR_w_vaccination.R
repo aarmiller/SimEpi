8b53feedb64fdc5dbe7938d5afb7edef959d00e0
aarmiller <34134737+aarmiller@users.noreply.github.com>
2021-05-10 01:46:32 -0500

    Create heterogeneous_SIR_w_vaccination.R

---
+++ in_class_scripts/part3/heterogeneous_SIR_w_vaccination.R
@@ -0,0 +1,354 @@
+
+rm(list=ls()) 
+library(tidyverse)
+
+##########################
+#### Helper Functions ####
+##########################
+
+## Flip a vector of weighted coins ---------------------------------------------
+
+flip_coin <- function(probs){
+  #as.logical(rbinom(length(probs),1,probs))
+  runif(length(probs))<probs
+}
+
+# flip_coin(c(0.1,0.3,0.5,0.6,0.9)) # example
+
+## Make our contract matrix ----------------------------------------------------
+
+make_cm <- function(agent_types, n_types, contact_probs) {
+  
+  n_agents <- sum(n_types)
+  
+  agents <- rep(agent_types,times = n_types)
+  
+  ## HCW -> HCW ----------------------------------------------------------------
+  
+  cm1 <- matrix(rep(contact_probs[1,1],times = n_types[1]^2),nrow = n_types[1])
+  
+  ## Patients -> Patients --------------------------------------------------------
+  
+  cm2 <- matrix(rep(contact_probs[2,2],times = n_types[2]^2),nrow = n_types[2])
+  
+  ## Patients -> HCWs (0.05) -----------------------------------------------------
+  cm3 <- matrix(rep(contact_probs[2,1],times = n_types[1]*n_types[2]),nrow = n_types[2])
+  
+  # combine submatrices
+  cm <- rbind(cbind(cm1,t(cm3)),
+              cbind(cm3,cm2))
+  
+  # fix diagonal
+  diag(cm) <- 0
+  
+  # optional: make symmetric (if not)
+  
+  # add names
+  row.names(cm) <- agents
+  colnames(cm) <- agents
+  
+  return(cm)
+}
+
+
+###############################
+#### Simulation Parameters ####
+###############################
+
+## Specify a simulation control function for holding parameters ----------------
+
+sim_ctrl <- function(n_types = c(500,300), agent_types = c("H","P"), n_days = 100, 
+                     susceptibility = c(.05,.15), infectivity = c(.65,.65),
+                     duration = 7, init_infect = 2, pct_vaccinated = c(.85, .5),
+                     ve = 0.9, contact_probs = NULL){
+  
+  # if the contact matrix is missing build it
+  if (is.null(contact_probs)) {contact_probs <- matrix(c(0.01,0.01,0.01,0.001),nrow = 2)}
+  
+  # constuct contact matrix
+  cm <- make_cm(agent_types = agent_types,
+                n_types = n_types,
+                contact_probs = contact_probs)
+  
+  # compute total agents
+  n_agents <- sum(n_types)
+  
+  # build output parameter list
+  list(n_agents = n_agents,             # Number of agents (patients or HCWs) to simulate
+       agent_types = agent_types,
+       n_types = n_types,
+       n_days = n_days,                 # Number of days to run simulation)
+       susceptibility = susceptibility, # how susceptible agents are
+       infectivity = infectivity,       # how infective agents are that are infected
+       duration = duration,             # how many periods the infection lasts
+       init_infect = init_infect,       # initial number of infections to seed
+       pct_vaccinated = pct_vaccinated, # percent of each agent type vaccinated
+       ve = ve,                         # vaccine efficacy
+       cm = cm)                         # contact matrix
+}
+
+sim_ctrl(susceptibility = c(0.05,0.25))
+sim_ctrl()
+# sim_ctrl(daily_contacts = 5) # example
+
+########################################
+#### Simulation Procedure Functions ####
+########################################
+
+## Build Patients --------------------------------------------------------------
+
+# Write a function that builds a set of agents for the simulator
+
+build_patients <- function(sim_params = sim_ctrl()){
+  
+  infect_index <- sample(sim_params$n_agents, size = sim_params$init_infect)
+  
+  # draw HCW (agent type 1) who are vaccinated
+  hcw_vac <- rbinom(n = sim_params$n_type[1], 
+                    size = 1, 
+                    prob = sim_params$pct_vaccinated[1])
+  
+  # draw patients(agent type 2) who are vaccinated
+  pat_vac <- rbinom(n = sim_params$n_type[2], 
+                    size = 1, 
+                    prob = sim_params$pct_vaccinated[2])
+  
+  
+  # construct data
+  tibble(agent = 1:sim_params$n_agents,
+         agent_type = rep(sim_params$agent_types, times = sim_params$n_types),
+         time = 1L,
+         state = ifelse(agent %in% infect_index, "I", "S"),
+         days_infected = ifelse(state == "I", 1L, 0L),
+         #days_infected = ifelse(state == "I", sample(1:duration,init_infect), 0L),
+         susceptibility = rep(sim_params$susceptibility, times = sim_params$n_types),
+         transmisibility = rep(sim_params$infectivity, times = sim_params$n_types),
+         vaccinated = c(hcw_vac,pat_vac),
+         trans_ind = NA)
+}
+
+# test that a random number are vaccinated
+build_patients() %>% 
+  group_by(agent_type) %>% 
+  summarise(mean(vaccinated))
+
+# build_patients() 
+
+# sim_ctrl(susceptibility = 0.2) %>% 
+#   build_patients() 
+
+
+## Make Interactions -----------------------------------------------------------
+
+# Write a function that makes the patients interact with one another and 
+# determines what transmission occurred
+make_interactions <- function(data, sim_params = sim_ctrl()){ 
+  
+  # Loop over agents
+  
+  # find infectious agents
+  infectious_index <- which(data$state == "I")
+  
+  # create transmission vector placeholder
+  trans_vec <- rep(FALSE, nrow(data))
+  
+  # looping over just infectious agents
+  for (i in infectious_index){
+    
+    #### for each agent j != i
+    
+    # susceptibility of j * infectivity of i * contact probability 
+    trans_prob <- data$susceptibility * data$transmisibility[i] * sim_params$cm[i,] * 
+      (1- data$vaccinated*sim_params$ve)
+    
+    # flip coin to determine transmissions
+    tmp_trans_vec <- flip_coin(trans_prob)
+    
+    # add to our overall transmission vector
+    trans_vec <- (trans_vec + tmp_trans_vec)>0
+  }
+  
+  # add transmission indicator into data
+  data %>% 
+    mutate(trans_ind = trans_vec)
+  
+}
+
+
+
+# build_patients() %>%
+#   make_interactions() %>%
+#   filter(trans_ind==TRUE)
+
+
+
+
+## Update States ---------------------------------------------------------------
+
+# Write a function that updates the states of all the agents in the simulator
+# when we reach the end of a timestep
+
+update_states <- function(data, sim_params = sim_ctrl()){
+  
+  # Update the the time step
+  # Update the state from I to R if they hit the duration threshold
+  out <- data %>% 
+    mutate(time = time + 1L) %>% 
+    mutate(state = ifelse(state == "I" & days_infected > sim_params$duration, "R", state))
+  
+  # add in the new infections from the trans_ind
+  out <- out  %>% 
+    mutate(state = ifelse(trans_ind==TRUE & state == "S", "I", state))
+  
+  # update the number of days infected
+  out <- out  %>% 
+    mutate(days_infected = ifelse(state == "I", days_infected+1L, 0L))
+  
+  return(out)
+}
+
+# build_patients() %>% 
+#   make_interactions() %>% 
+#   update_states() 
+
+############################################
+#### Main Simulation Procedure Function ####
+############################################
+
+## Build Single Simulation Function --------------------------------------------
+
+# Write a function that incorporates the above steps to run through a single 
+# trial of the simulation.
+
+# Consider an intermediate function that computes statistics of interest at each
+# stage of the simulation, then aggregate this into an output object
+
+run_single_sim <- function(sim_params = sim_ctrl(), output_final_state = FALSE){
+  
+  # initial patients
+  patients <- build_patients(sim_params = sim_params)
+  
+  # keep track of agent state history
+  agent_history <- patients %>% 
+    count(state, agent_type) %>% # update to count type
+    mutate(time = 0L)
+  
+  for (i in 1:sim_params$n_days) {
+    
+    # update patient states
+    patients <- patients %>% 
+      make_interactions(sim_params = sim_params) %>% 
+      update_states(sim_params = sim_params)
+    
+    # update patient history
+    tmp_agent_history <- patients %>% 
+      count(state, agent_type) %>%  # update to count type
+      mutate(time = i)
+    
+    # append patient history
+    agent_history <- rbind(agent_history,tmp_agent_history)
+    
+    # how many infectious
+    n_infectious <- sum(patients$state=="I")
+    
+    # stop simulation if no more infections 
+    if (n_infectious == 0) { break }
+    
+  }
+  
+  if (output_final_state){
+    
+    # return output with final state
+    return(list(agent_history = agent_history,
+                final_state = patients))
+    
+  } else {
+    
+    return(agent_history)
+    
+  }
+  
+}
+
+# run_single_sim(output_final_state = TRUE)
+
+tmp <- run_single_sim(output_final_state = FALSE)
+
+run_single_sim(output_final_state = FALSE) %>% 
+  ggplot(aes(time, n, color = state)) +
+  geom_line() +
+  facet_wrap(~agent_type)
+
+## Build Multiple Trial Simulation Function ------------------------------------
+
+# Write final simulation function that allows for multiple trials (later you may
+# want to rewrite this to run in parallel)
+
+run_sim <- function(trials = 10, sim_params = sim_ctrl()){
+  
+  tibble(trials = 1:trials) %>% 
+    mutate(results = map(trials, ~run_single_sim(sim_params = sim_params))) %>% 
+    unnest(cols = c(results))
+  
+}
+
+res <- run_sim(sim_params = sim_ctrl(susceptibility = 0.15))
+
+## Parallel Version of Run Sim -------------------------------------------------
+
+library(parallel)
+detectCores(logical = FALSE)
+
+# add parLapply implementation
+
+run_sim_mc <- function(trials = 10, sim_params = sim_ctrl(), cores = 4){
+  
+  tmp <- mclapply(1:trials,
+                  function(x) run_single_sim(sim_params = sim_params), 
+                  mc.cores = cores)
+  
+  res <- enframe(tmp,name = "trials") %>% 
+    unnest(cols = c(value))
+  
+  return(res) 
+}
+
+# generate results for 100 trials using 5 cores
+tmp <- run_sim_mc(trials = 100, cores = 5)
+
+
+############################################
+#### Visualize Results for different VE ####
+############################################
+
+# generate results for different levels of vaccine efficacy
+
+res_ve_high <- run_sim_mc(trials = 500, cores = 5,
+                          sim_params = sim_ctrl(ve = 0.9))
+
+
+
+res_ve_low <- run_sim_mc(trials = 500, cores = 5,
+                          sim_params = sim_ctrl(ve = 0.2))
+
+# combine results
+
+combined_results <- bind_rows(mutate(res_ve_high,
+                 ve_level = "high"),
+          mutate(res_ve_low,
+                 ve_level = "low"))
+
+# visualize the average trend
+combined_results %>% 
+  pivot_wider(names_from = state, 
+              values_from = n,
+              values_fill = 0) %>% 
+  group_by(time,ve_level) %>% 
+  summarise_at(vars(I:R), list(mean)) %>% 
+  pivot_longer(cols = I:R,
+               names_to = "state",
+               values_to = "n") %>% 
+  ggplot(aes(x = time, y = n, color = state)) +
+  geom_line() +
+  facet_wrap(~ve_level)
+
